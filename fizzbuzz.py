574d5e512f90352aa6b86974dbe0a140b54af20c
Manish Bhobe <mjbhobe@gmail.com>
2023-08-27 18:43:42 +0530

    New Code Example

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -46,7 +46,7 @@ print("Using Pytorch version: ", torch.__version__)
 # My helper functions for training/evaluating etc.
 import torch_training_toolkit as t3
 
-SEED = t3.seed_all(123)
+SEED = t3.seed_all(41)
 
 logger = logging.getLogger(__name__)
 
@@ -85,20 +85,25 @@ class Net(nn.Module):
         self.net = nn.Sequential(
             nn.Linear(inp, hidden),
             nn.ReLU(),
+            nn.Linear(hidden, 2 * hidden),
+            nn.ReLU(),
+            nn.Linear(2 * hidden, hidden),
+            nn.ReLU(),
             nn.Linear(hidden, out),
             nn.Sigmoid()
         )
 
-    def _slow_forward(self, inputs):
+    def forward(self, inputs):
         return self.net(inputs)
 
 
 class FizzBuzzDataset(Dataset):
-    input_size : int = 10
-    start : int = 0
+    input_size: int = 10
+    start: int = 0
 
     def __init__(self, max_num):
         self.end = max_num
+        self.input_size = len([int(i) for i in '{0:b}'.format(max_num)])
 
     def encoder(self, num):
         ret = [int(i) for i in '{0:b}'.format(num)]
@@ -108,31 +113,38 @@ class FizzBuzzDataset(Dataset):
         idx += self.start
         x = self.encoder(idx)
         if idx % 15 == 0:
-            y = [1,0,0,0]
+            y = [1, 0, 0, 0]
         elif idx % 5 == 0:
-            y = [0,1,0,0]
+            y = [0, 1, 0, 0]
         elif idx % 3 == 0:
-            y = [0,0,1,0]
+            y = [0, 0, 1, 0]
         else:
-            y = [0,0,0,1]
-        return x, y
+            y = [0, 0, 0, 1]
+        return torch.Tensor(x), torch.Tensor(y)
 
     def __len__(self):
         return self.end - self.start
+
+
+MAX_NUM = 100_000_000
 
 
 def main():
     parser = t3.TrainingArgsParser()
     args = parser.parse_args()
 
-    dataset = FizzBuzzDataset(100000)
-    train_dataset, eval_dataset = t3.split_dataset(dataset, split_perc=args.val_split)
-    eval_dataset, test_dataset = t3.split_dataset(eval_dataset, split_perc=args.test_split)
+    dataset = FizzBuzzDataset(MAX_NUM)
+    train_dataset, test_dataset = t3.split_dataset(dataset, split_perc=args.test_split)
+    train_dataset, eval_dataset  = t3.split_dataset(train_dataset, split_perc=args.val_split)
     print(f"train_dataset: {len(train_dataset)} recs - eval_dataset: {len(eval_dataset)} recs - "
           f"test_dataset: {len(test_dataset)} recs")
-    print(len(train_dataset[0][0]), len(train_dataset[0][1]))
+    # print(len(train_dataset[0][0]), len(train_dataset[0][1]))
+    # testloader = torch.utils.data.DataLoader(test_dataset, batch_size=16, shuffle=True)
+    # data_iter = iter(testloader)
+    # x_batch, y_batch = next(data_iter)  # fetch first batch of 64 images & labels
+    # print(x_batch, y_batch)
+    # print(f"x_batch.shape: {x_batch.shape} - y_batch.shape: {y_batch.shape}")
     # sys.exit(-1)
-
 
     # X, y = get_data()
     # print(f"x[:5] = {X[:5]}")
@@ -153,34 +165,81 @@ def main():
     # create our module
     loss_fn = nn.CrossEntropyLoss()
     metrics_map = {
-        "acc" : MulticlassAccuracy(4)
+        "acc": MulticlassAccuracy(4)
     }
     trainer = t3.Trainer(
-        loss_fn = loss_fn,
-        device = DEVICE,
-        epochs = args.epochs,
-        batch_size = args.batch_size,
+        loss_fn=loss_fn,
+        device=DEVICE,
+        metrics_map=metrics_map,
+        epochs=args.epochs,
+        batch_size=args.batch_size,
     )
 
+    def create_model(max_num: int = MAX_NUM) -> nn.Module:
+        inp_nodes = len([int(i) for i in '{0:b}'.format(max_num)])
+        net = Net(inp_nodes, 128, 4)
+        return net
+
     if args.train:
-        net = Net(14, 100, 4)
+        net = create_model()
         optimizer = torch.optim.Adam(net.parameters(), lr=args.lr)
+        # add L1 regularization & a learning scheduler
+        from torch.optim.lr_scheduler import StepLR
+        scheduler = StepLR(optimizer, step_size=args.epochs // 5, gamma=0.1, verbose=True)
+
         hist = trainer.fit(
             net,
             optimizer,
             train_dataset,
             validation_dataset=eval_dataset,
+            lr_scheduler=scheduler,
         )
         hist.plot_metrics(title="FizzBuzz Model Performance")
         t3.save_model(net, MODEL_SAVE_PATH)
         del net
 
     if args.eval:
-        pass
+        net = create_model()
+        net = t3.load_model(net, MODEL_SAVE_PATH)
+        print("Evaluating model performance...")
+        metrics = trainer.evaluate(net, train_dataset)
+        print(
+            f" - Training -> loss: {metrics['loss']:.4f} - acc: {metrics['acc']:.4f}"
+        )
+        metrics = trainer.evaluate(net, eval_dataset)
+        print(
+            f" - Cross-val -> loss: {metrics['loss']:.4f} - acc: {metrics['acc']:.4f}"
+        )
+        metrics = trainer.evaluate(net, test_dataset)
+        print(
+            f" - Testing -> loss: {metrics['loss']:.4f} - acc: {metrics['acc']:.4f}"
+        )
+        del net
 
     if args.pred:
-        pass
+        net = create_model()
+        net = t3.load_model(net, MODEL_SAVE_PATH)
+        print("Running predictions...")
+        preds, actuals = trainer.predict(net, test_dataset)
+        preds, actuals = np.argmax(preds, axis=1), np.argmax(actuals, axis=1)
+        print(f"preds -> {preds[:50]}")
+        print(f"preds -> {actuals[:50]}")
+        incorrect_counts = (preds != actuals).sum()
+        print(f"We got {incorrect_counts} of {len(preds)} incorrect!")
+        del net
 
 
 if __name__ == "__main__":
     main()
+
+# ---------------------------------------------------
+# Model Performance:
+#   Epochs: 100, Batch-size: 64, LR: 0.01 (with step)
+# Training dataset  ->  loss: 1.2067 - acc: 0.6998
+# Cross-val dataset ->  loss: 1.2044 - acc: 0.7013
+# Testing dataset   ->   loss: 1.2077 - acc: 0.6991
+#
+# Conclusion:
+#   Model is not overfitting, but we are not getting
+#   good performance (only ~70% accuracy)
+# ----------------------------------------------------
